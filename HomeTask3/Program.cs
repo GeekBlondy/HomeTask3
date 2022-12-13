451b4d48e1e4478eb851e153fafb63f09ad69561
Elena <emwatercolor@gmail.com>
2022-12-13 19:44:39 +0300

    Therd nask

--- HomeTask3/Program.cs
+++ HomeTask3/Program.cs
@@ -1,16 +1,43 @@
 ﻿// 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
+bool Palindrom (int N)
+{
+
+
+
+}
+
+Console.WriteLine("Input a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Palindrom (N);
 
 
 
 // 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
+/*
+double DistanceAB (double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    double dist = Math.Sqrt(Math.Pow(xA-xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA-zB, 2));
+}
 
+Console.WriteLine("Input xA: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input xB: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input yA: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input yB: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input zA: ");
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input zB: ");
+double zB = Convert.ToDouble(Console.ReadLine());
 
-
-
-
-
+double dist = Math.Round(DistanceAB(xA, yA, zA, xB, yB, zB), 3);
+Console.WriteLine("A distance is: {dist}");
+*/
 
 
 
